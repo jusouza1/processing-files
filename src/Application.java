56a36b55e6dda4b0aa56f9a6abf6d9e7c928b7df
Juliana <julianasouzamelo@live.com>
2023-07-14 19:04:11 -0300

    autocloseable

--- src/Application.java
+++ src/Application.java
@@ -5,11 +5,15 @@ public class Application {
     public static void main(String[] args) {
 
         File file = new File("myfilfdfe.txt");
-        BufferedReader bufferedReader = null;
-        FileReader fileReader = null;
-        try {
-            fileReader = new FileReader(file);
-            bufferedReader = new BufferedReader(fileReader);
+        /*
+         try with resources -> java 7
+         -> automatically closes the resources
+         -> any class that implements autocloseable can be used in this try-with-resources block
+         -> autocloseable is an interface and can be extended by the class closeable
+         -> its possible to create a class that implements autocloseable
+         */
+        try (FileReader fileReader = new FileReader(file);
+             BufferedReader bufferedReader = new BufferedReader(fileReader)){
             /*
             bufferedReader -> reads the file line by line
             fileReader -> reads all lines from file at once
@@ -23,18 +27,6 @@ public class Application {
             System.out.println("File not found");
         } catch (IOException e) {
             System.out.println("Problem reading the file " + file.getName());
-        } finally {
-            /*
-            always runs
-             */
-            try {
-                if (bufferedReader!= null)
-                    bufferedReader.close();
-                if (fileReader != null)
-                    fileReader.close();
-            } catch (IOException e) {
-                System.out.println("unable to close file" + file.getName());
-            }
         }
 
     }
